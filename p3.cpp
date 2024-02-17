2bf89996c46fd43a356bfb8e4f1aae10b3b24b15
IshitaSurati <ishitasurati83@gmail.com>
2024-02-17 20:20:13 +0530

    file

---
+++ p3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+/*control structure :
+1. selection
+2. sequential
+3. repeation
+
+1.selection structure:
+- if statement
+- if else
+- ladder
+*/
+
+int main()
+{
+
+    int a, b;
+    cout << "Enter A : ";
+    cin >> a;
+    cout << "Enter B : ";
+    cin >> b;
+
+    cout << "--: Find max number between two numbers :--" << endl;
+
+    cout << endl
+         << "-------if statement-------" << endl;
+    cout << "A : " << a << endl;
+    cout << "B : " << b << endl;
+    if (a > b)
+    {
+        cout << "A is max!" << endl;
+    }
+
+    cout << endl
+         << "-------if else statement-------" << endl;
+    cout << endl
+         << "Enter A : ";
+    cin >> a;
+    cout << "Enter B : ";
+    cin >> b;
+    cout << "A : " << a << endl;
+    cout << "B : " << b << endl;
+    if (a > b)
+    {
+        cout << "A is max!" << endl;
+    }
+    else
+    {
+        cout << "B is max!" << endl;
+    }
+}
